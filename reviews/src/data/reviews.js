69b33ecebc4832183d731946778462935f32da8b
Teri Eyenike <teyenike1@gmail.com>
2023-07-09 19:12:47 +0100

    Add: suggestions data from GPT

---
+++ reviews/src/data/reviews.js
@@ -0,0 +1,26 @@
+export const reviews = [
+  {
+    id: 1,
+    reviewerName: "Shyam Nath R S.",
+    reviewText:
+      "As always with Jonas's other courses like JS, HTML and CSS I understood everything he told. As an improvement, it was a bit uneasy seeing the date you wrote code is 2019 in some lectures. Wished it was brand new. So can you do some update. Also can you consider incorporating lectures about Websocket, GraphQL, Testing, Node.js with TypeScript, and Deno as these were some topics I see included in another popular Node.js course. Anyways above are just suggestions I could think of and I am 99% happy with this course. Hope Jonas also does courses on mobile app development like React Native, Flutter, or Kotlin as those are things I want to learn next. Thank you Jonas!!",
+    improvementSuggestions: [
+      "Update course content to be more current",
+      "Include lectures on Websocket, GraphQL, Testing, Node.js with TypeScript, and Deno",
+      "Consider creating courses on mobile app development with React Native, Flutter, or Kotlin",
+    ],
+  },
+  {
+    id: 2,
+    reviewerName: "Yash U.",
+    reviewText:
+      "This was a very intensive course covering almost all backend stuff. A huge thanks to the instructor - Jonas and also to the community. A lot of bugs and problems were already posted in the Q&A section and it helped a lot. Towards the end of the course, there were a few things that were outdated and a lot of people were disappointed in the comments but for me these things helped a lot. You learn to search and find solutions on your own and this is what is required in real world. Hence, despite these issues towards the end, I would absolutely recommend this course to anyone who wants to start learning backend development.",
+    improvementSuggestions: [
+      "Consider adding specific examples of the backend concepts covered in the course.",
+      "Try to provide a balanced feedback by addressing both positive and negative aspects of the course.",
+      "Expand on how the course helped you develop your backend skills and provide specific examples of projects or challenges you were able to tackle as a result.",
+      "Share your insights on the teaching methods, course structure, and interactive elements that enhanced your learning experience.",
+      "Consider acknowledging the experiences of other learners and provide a broader perspective in your review.",
+    ],
+  },
+];
