4acd5195d5c054ccdd057518f1cf02fd02dc9758
Teri Eyenike <teyenike1@gmail.com>
2023-07-09 21:20:24 +0100

    Add: importing modules and environment variables

--- headless-web-scraping/index.js
+++ headless-web-scraping/index.js
@@ -1,26 +1,7 @@
 const puppeteer = require("puppeteer-core");
-const auth = "brd-customer-hl_05f7a03a-zone-extract_reviews:h7izen1ms7e8";
+require("dotenv").config();
+const fs = require("fs");
 
-async function run() {
-  let browser;
-  try {
-    browser = await puppeteer.connect({
-      browserWSEndpoint: `wss://${auth}@brd.superproxy.io:9222`,
-    });
-    const page = await browser.newPage();
-    page.setDefaultNavigationTimeout(2 * 60 * 1000);
-    await page.goto(
-      "https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/"
-    );
-    const html = await page.content();
-    const title = await page.evaluate(() => document.title);
-    console.log(html);
-    console.log(title);
-  } catch (e) {
-    console.error("run failed", e);
-  } finally {
-    await browser?.close();
-  }
-}
+const auth = process.env.AUTH;
+const host = process.env.HOST;
 
-if (require.main == module) run();
