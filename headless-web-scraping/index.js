5a03ea526b579dfabad8c98c90be264868035c84
Teri Eyenike <teyenike1@gmail.com>
2023-07-09 21:35:59 +0100

    Update: proxy network setup to scrape udemy reviews

--- headless-web-scraping/index.js
+++ headless-web-scraping/index.js
@@ -5,3 +5,23 @@ const fs = require("fs");
 const auth = process.env.AUTH;
 const host = process.env.HOST;
 
+async function run() {
+  let browser;
+  try {
+    browser = await puppeteer.connect({
+      browserWSEndpoint: `wss://${auth}@${host}`,
+    });
+    const page = await browser.newPage();
+    page.setDefaultNavigationTimeout(2 * 60 * 1000);
+    await page.goto(
+      "https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/"
+    );
+  } catch (error) {
+
+  }
+  finally {
+    await browser?.close()
+  }
+}
+
+if (require.main == module) run();
